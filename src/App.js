adc09ea105693f7c16247df040ee36af65d15323
Yurbanis <yurbanisbriceno@gmail.com>
2021-09-07 20:37:47 -0300

    tarea 2
    Se agrega Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
     return ( <
-        div className = "App" >
+        div >
         <
-        header className = "App-header" >
+        NavBar / >
         <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Mi empresa Bricash. <
-        /p> <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
         /div>
     );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function NavBar() {
+    return ( <
+        div >
+        <
+        nav className = "navbar-dark bg-dark" >
+        <
+        div className = "container" >
+        <
+        img src = "../../public/logo.jpn" >
+        <
+        a className = "navbar-brand"
+        href = "#" > BRICASH < /a> 
+
+        <
+        div className = "d-flex justify-content-end" >
+        <
+        a className = "navbar-brand"
+        href = "#" > Inicio < /a>   <
+        a className = "navbar-brand"
+        href = "#" > Nosotros < /a> <
+        a className = "navbar-brand"
+        href = "#" > Contacto < /a> < /
+        div >
+
+        <
+        /div > < /
+        nav > <
+        /div>
+    )
+}
